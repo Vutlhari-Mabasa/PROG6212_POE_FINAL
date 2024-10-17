5df8f01c0d5f5b9db15991e86647f0e0fb22f757
unknown <ST10250190@vcconnect.edu.za>
2024-10-17 19:21:21 +0200

    registered the database service

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<HRContext>(
+              options => options.UseSqlServer(builder.Configuration.GetConnectionString("Name")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
